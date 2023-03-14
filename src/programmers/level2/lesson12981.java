e6a16c9d37de7dbe873faed339e71a8d3a7c0eca
Minwoo Chase <c5287@nate.com>
2023-03-14 16:16:48 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/12981?language=java 영어 끝말잇기

--- src/programmers/level2/lesson12981.java
+++ src/programmers/level2/lesson12981.java
@@ -1,31 +1,27 @@
 package programmers.level2;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
 public class lesson12981 {
     public static int[] solution(int n, String[] words) {
         int[] answer = {0, 0};
-
-        //1. 사용한 단어를 확인하기위해 Set 변수선언
         Set<String> usedWords = new HashSet<>();
-        //2. 마지막글자를 가져오기위한 변수선언.
-        char lastChar = words[0].charAt(0);
-        int count = 1;
+        char lastChar = ' ';
 
         for (int i = 0; i < words.length; i++) {
-            String word = words[i];
-            if (i != 0) {
-                if (lastChar != word.charAt(0) || usedWords.contains(word)) {
-                    answer[0] = (i % n) + 1;
-                    answer[1] = (i / n) + 1;
-                    break;
-                }
+            if (i != 0 && (lastChar != words[i].charAt(0) || usedWords.contains(words[i]))) {
+                answer[0] = (i % n) + 1;
+                answer[1] = (i / n) + 1;
+                break;
             }
-            usedWords.add(word);
-            lastChar = word.charAt(word.length() - 1);
-            count++;
+            usedWords.add(words[i]);
+            lastChar = words[i].charAt(words[i].length() - 1);
         }
+
+        System.out.println(Arrays.toString(answer));
+
         return answer;
     }
 
